2c6f28175b13cdcf37365e41f46012f798b3c51c
unknown <mhung4011@gmail.com>
2023-10-09 09:56:15 +0700

    Day31

---
+++ AspNetCore/Lession01/AspNetCoreMVC-Ex/AspNetCoreMVC-Ex/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreMVC_Ex.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

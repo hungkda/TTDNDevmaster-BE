4f8666390607b5a96eaeb581db91ca92451730f1
unknown <mhung4011@gmail.com>
2023-11-16 11:06:23 +0700

    Day39

---
+++ AspNetCore/Lession08/Lession08.Lab/Lession08.Lab/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lession08.Lab.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

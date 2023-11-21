143f195733a221e1b6d3fca937abf9b724886790
unknown <mhung4011@gmail.com>
2023-11-21 11:47:40 +0700

    Da42

---
+++ AspNetCore/Lession09/Lab09/Lab09/Areas/Admins/Controllers/LoginController.cs
@@ -0,0 +1,63 @@
+﻿using Lab09.Areas.Admins.Models;
+using Lab09.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Lab09.Areas.Admins.Controllers
+{
+    [Area("Admins")]
+    public class LoginController : Controller
+    {
+        private readonly DevXuongMocContext _context;
+        public LoginController(DevXuongMocContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Login model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model); // trả về trạng thái lỗi
+            }
+
+            //xử lý login đăng nhập
+            var pass = GetSHA256Hash(model.Password);
+            var dataLogin = _context.AdminUsers.Where(x => x.Email.Equals(model.Email) && x.Password.Equals(pass)).FirstOrDefault();
+
+            if(dataLogin != null)
+            {
+                // lưu session khi đăng nhập thành công
+                HttpContext.Session.SetString("AdminLogin", model.Email);
+                return RedirectToAction("Index", "Dashboard");
+            }
+
+            return View(model); // trả về trạng thái lỗi
+        }
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Remove("AdminLogin"); //Huyr session voi key AdminLogin
+            return RedirectToAction("Index");
+        }
+
+        static string GetSHA256Hash(string input)
+        {
+            string hash = "";
+            using(var sha256 = new SHA256Managed())
+            {
+                var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+                hash = BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+            }
+            return hash;
+        }
+    }
+}

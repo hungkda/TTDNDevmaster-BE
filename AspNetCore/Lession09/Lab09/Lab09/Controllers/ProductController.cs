7eb18f8d96b7a504d94f3493d7551660beabdc64
unknown <mhung4011@gmail.com>
2023-11-23 15:09:42 +0700

    Day43

---
+++ AspNetCore/Lession09/Lab09/Lab09/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Lab09.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab09.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly DevXuongMocContext _content;
+        public ProductController(DevXuongMocContext content)
+        {
+            _content = content;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var data = await _content.Products.ToListAsync();
+            return View(data);
+        }
+    }
+}
